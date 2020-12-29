8c356246c8b9e4d55cf1e040233129c91948ed90
Catalina Gligor <catalinaanamaria56@gmail.com>
2020-12-29 13:38:03 +0200

    app.js modified

--- backend/app.js
+++ backend/app.js
@@ -10,27 +10,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-/*
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerUI = require('swagger-ui-express');
-
-const swaggerOptions = {
-    swaggerDefinition: {
-        info: {
-            title: "Shop API",
-            description: "Backend Api",
-            contact: {
-                name: 'Amazing Developer'
-            },
-            servers: "http://localhost:3000"
-        }
-    },
-    apis: ["app.js", ".routes/*.js"]
-};
-
-const swaggerDocs = swaggerJSDoc(swaggerOptions);
-app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
-*/
 
 app.use(cors( {
   origin : "*",
@@ -49,26 +28,7 @@ app.use('/api/users', usersRoute);
 app.use('/api/orders', ordersRoute);
 
 
-;/*
-app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
-*/
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
